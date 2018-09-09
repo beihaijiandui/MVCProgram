deddac94fc9b3cd3de7e112b04130081f3704ce1
beihaijiandui <xinchangxin1990@hotmail.com>
2018-09-10 00:03:50 +0800

    User

---
+++ MVCProgram/MVCApp/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCApp.Models
+{
+    public class User
+    {
+        public string Name
+        {
+            set;
+            get;
+        }
+    }
+}
