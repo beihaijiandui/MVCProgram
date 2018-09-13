67fbc1e06ba0a84c64a6a8f95641170ab25b6335
beihaijiandui <xinchangxin1990@hotmail.com>
2018-09-13 23:41:52 +0800

    httpclient get

---
+++ MVCProgram/WebApiClient/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using WebApiClient.Models;
+
+namespace WebApiClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            HttpClient client = new HttpClient();
+            client.DefaultRequestHeaders.Accept.Add
+                (new MediaTypeWithQualityHeaderValue("application/json"));
+
+            //执行Get操作
+            HttpResponseMessage response=client.GetAsync("http://localhost:50047/api/Products").Result;
+            var list = response.Content.ReadAsAsync<List<Product>>().Result;
+            ViewData.Model = list;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
